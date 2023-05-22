fcf991215b59b0fabac16246db683de2d08dbef0
BT_3W2_15 <wendyz34@nycstudents.net>
2023-05-22 09:27:08 -0400

    ...

--- src/ProductionPossibilityCurve.java
+++ src/ProductionPossibilityCurve.java
@@ -1,24 +1,26 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.geom.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.awt.event.MouseEvent;
 
-public class ProductionPossibilityCurve extends JPanel {
+public class ProductionPossibilityCurve extends JPanel{
 
     private List<Point> points;
     private String xAxisLabel;
     private String yAxisLabel;
     private static int con;
     private static int cap;
+    private boolean ovalClicked;
 
     public ProductionPossibilityCurve(String xAxisLabel, String yAxisLabel) {
         points = new ArrayList<>();
         this.xAxisLabel = xAxisLabel;
         this.yAxisLabel = yAxisLabel;
     }
-
     public void addPoint(int x, int y) {
         points.add(new Point(x, y));
     }
@@ -31,9 +33,8 @@ public class ProductionPossibilityCurve extends JPanel {
         // Set up the coordinate system
         int width = getWidth();
         int height = getHeight();
-        int xCenter = width / 2;
-        int yCenter = height / 2;
-
+        int xCenter = width/2;
+        int yCenter = height/2;
         int thickness = 7;
          ((Graphics2D) g).setStroke(new BasicStroke(thickness));
         //Draw the vertical line
@@ -41,8 +42,6 @@ public class ProductionPossibilityCurve extends JPanel {
         g.drawLine(width / 2 ,0, width/2 , height / 2);
         // Draw the horizontal line
         g.drawLine(width/2, height/2, width,height/2);
-
-
         // Draw the x and y axis labels
         g.drawString(xAxisLabel, 250, 300);
         g.drawString(yAxisLabel, 100, 100);
@@ -53,39 +52,43 @@ public class ProductionPossibilityCurve extends JPanel {
             int x = xCenter + point.x;
             int y = yCenter - point.y;
             ((Graphics2D) g).setStroke(new BasicStroke(thickness));
-            g.fillOval(x - 2, y - 2, 10, 10);
+           g.fillOval(x - 2, y - 2, 10, 10);
         }
 
         // Draw the slope
         g.setColor(Color.DARK_GRAY);
         ((Graphics2D) g).setStroke(new BasicStroke(4));
-        //int y2 = (int) ((width/2)-(slopeRatio * (350-width/2)));
-        g.drawLine(width,cap/2,con/2, 0);
-        //g.drawArc(width/2,height/2,300,300,0,90);
-        //g.drawLine(width,height/2,width/2,0);
+        g.drawLine(width, con/2, cap/2,0);
+        //g.drawArc(0,con/2,500,500,0,90);
+        g.drawArc(cap/2,0,400,400,0,90);
+       // g.drawArc();
 
 
+
+    }
+    public void mousePressed(MouseEvent e){
 
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Production Possibility Curve");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(550, 550);
         Scanner scanner = new Scanner(System.in);
         //label axis
+        //coordinates only show within 250?
         String xAxisLabel= JOptionPane.showInputDialog("Label x:");
         String yAxisLabel= JOptionPane.showInputDialog("Label y:");
         ProductionPossibilityCurve graphPanel = new ProductionPossibilityCurve(xAxisLabel, yAxisLabel);
         //get slope
+        //max production only show within 500
         String slope = JOptionPane.showInputDialog("During a normal economic time what's the max production of consumer goods?");
          con = Integer.parseInt(slope);
         String s = JOptionPane.showInputDialog("Max production of consumer goods?");
          cap = Integer.parseInt(s);
         //get coordinates
         while (true) {
-            System.out.print("Enter coordinates (x, y) separated by spaces (or press enter to finish): ");
-            String input = scanner.nextLine();
+            String input = JOptionPane.showInputDialog("Enter coordinates (x, y) separated by spaces (or press enter to finish): ");
             if (input.isEmpty()) {
                 break;
             }
